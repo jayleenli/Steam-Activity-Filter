42a69adacd035c62b6c80e1ea232cc343b299f33
Jayleen Li <jayleenli17@gmail.com>
2017-03-30 15:23:39 -0500

    Removed Chrome Storage API saving feature
    (Since I don't know how to encrypt properly on chrome extension)

--- steamfilter.js
+++ steamfilter.js
@@ -1,36 +1,5 @@
 var console_info = ["%cSteam Activity Filter by tjtheturtleisawesome(Jayleen Li)%c", "", ""];
 console.log.apply(console, console_info);
-/* 
-	READ ME
-	Made by tjtheturtleisawesome(Jayleen Li)
-	Steam Profile: http://steamcommunity.com/id/tjtheturtleisawesome/
-	GitHub: https://github.com/jayleenli
-	
-	Version 1.0 Features
-	-Filtering your Steam Activity by friends list!
-	-Hides the following types of posts from unselected users
-		*Screenshots
-		*New Games
-		*Status Posts
-		*Favorites
-	-Storing your filter preferences in Chrome Storage
-	-Works for Steam users with or without personalized Steam IDs
-	
-	Side Note: Why does steam use an old Jquery? makes this so much harder :)
-	Mini ID's for steam are used because...
-		-Not everyone has a steam ID set up
-		-Users with steam IDs also have profile links, which can mess up coding
-		-Steam mini ID's are the only consistent ID (and easier to get) to use
-	
-	Known Issues or Things to Improve on
-	-Issue: Inefficient coding(but it still works!)
-	-Issue: Does not work for next day Steam Activity (when it loads more activity when you scroll down)
-	-Issue: Does not filter smaller info (ex, new friends, achievements, groups joined)
-	-Future Feature?: Filter for groups
-	-Future Feature?: Search for specific user
-	-Future Feature?: Filter for the smaller info 
-	-Future Feature?: Select All option/Deselect all option
-*/
 
 /*
 	Creates the interface for Steam Activity Filter, places it on Steam Activity Page
@@ -38,15 +7,20 @@ console.log.apply(console, console_info);
 var div = document.createElement('div');
 div.className = 'blotter_day';
 document.getElementById('blotter_statuspost_form').appendChild(div);
-
 /*
 	Instantation variables
+	Encryption Set Up
 */
 var userFriendList = []; //array by [steam MINI profile ID]
 var HTMLtoadd = ""; //for the list of friends on the filter
 var usersToHideChromeStorage; 
 var SAFslideshowDivs = [];
 var currentlydisplayed = 1;
+
+var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase"); 
+var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+ 
+console.log(plaintext);
 
 /*
 	Get the friends link of the steam user
